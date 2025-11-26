e242f333936b36e26c062df9ecc72a32165290cf
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2025-11-26 16:35:48 +0100

    init: first experiment of laravel jetstream + fortify + livewire

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+<x-guest-layout>
+    <h1 class="text-black font-bold  text-6xl"> Heading 1 </h1>
+    <h2 class="text-black font-bold  text-5xl"> Heading 1 </h2>
+    <h3 class="text-black font-bold  text-4xl"> Heading 1 </h3>
+    <h4 class="text-black font-bold  text-3xl"> Heading 1 </h4>
+    <h4 class="text-black font-bold  text-2xl"> Heading 1 </h4>
+    <p class="text-black">fadkdjlkasfkadls</p>
+    <p class="font-heading1">Hello</p>
+</x-guest-layout>
