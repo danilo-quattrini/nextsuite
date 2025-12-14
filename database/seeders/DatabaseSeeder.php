9f7c343c4d44bdeba002286225f6d0d20198cb3f
danilo.quattrini <daniloquattrini.com@gmail.com>
2025-12-14 13:16:24 +0100

    feat(field-category-seeder): create final connection with the FieldCategorySeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,5 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call([
+            CategorySeeder::class,
+            FieldSeeder::class,
+            FieldCategorySeeder::class,
+        ]);
     }
 }
---
+++ database/seeders/FieldCategorySeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use App\Models\Field;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class FieldCategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categoryFieldMap = [
+            'Health' => [
+                'Gym',
+                'Nutrition',
+                'Healthcare',
+                'Hospitality',
+            ],
+            'Technology' => [
+                'Artificial Intelligence',
+                'Software',
+            ],
+            'Business' => [
+                'Finance',
+                'Marketing',
+                'Human Resources',
+                'Hospitality',
+            ],
+        ];
+
+        foreach ($categoryFieldMap as $categoryName => $fieldNames) {
+
+            $category = Category::where('name', $categoryName)->first();
+
+            if (! $category) {
+                continue;
+            }
+
+            $fieldIds = Field::whereIn('name', $fieldNames)->pluck('id');
+
+            $category->fields()->syncWithoutDetaching($fieldIds);
+        }
+    }
+}
