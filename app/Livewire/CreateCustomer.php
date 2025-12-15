b619960c2b3d8c16f548ef975281a0271a07dff5
danilo.quattrini <daniloquattrini.com@gmail.com>
2025-12-15 11:04:56 +0100

    fix(customer-upload): move upload customer profile picture from the form component to the CreateCustomer component.

--- app/Livewire/CreateCustomer.php
+++ app/Livewire/CreateCustomer.php
@@ -5,11 +5,21 @@ namespace App\Livewire;
 use App\Livewire\Forms\CustomerForm;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\View\View;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class CreateCustomer extends Component
 {
+    use WithFileUploads;
+
     public CustomerForm $form;
+
+    #[Validate('required', message: 'Customer profile photo it\'s required.')]
+    #[Validate('mimes:jpeg,png,jpg,gif', message: 'Customer profile photo should be  one of this formats: jpeg,png,jpg,gif.')]
+    #[Validate('image', message: 'The file must be an image.')]
+    #[Validate('max:2048', message: 'Profile image it\'s too large.')]
+    public $customer_photo;
 
     public $step = 1;
 
@@ -31,8 +41,12 @@ class CreateCustomer extends Component
     {
         $this->form->validate();
 
+        $imageName = strtolower(str_replace(' ', '_', $this->form->full_name)) . '.' . $this->customer_photo->extension();
+        $this->customer_photo->storeAs('customers-profile-photos', $imageName, 'public');
+
         Customer::create([
             'name'  => $this->form->full_name,
+            'profile_photo_url' => $imageName,
             'email' => $this->form->email,
             'phone' => $this->form->phone,
             'dob'   => $this->form->dob,
--- app/Livewire/Forms/CustomerForm.php
+++ app/Livewire/Forms/CustomerForm.php
@@ -4,14 +4,9 @@ namespace App\Livewire\Forms;
 
 use Livewire\Attributes\Validate;
 use Livewire\Form;
-use Livewire\WithFileUploads;
 
 class CustomerForm extends Form
 {
-    use WithFileUploads;
-    #[Validate('required|image|mimes:jpeg,png,jpg,gif', message: 'Customer profile phot it\'s required')]
-    #[Validate('max:2048', message: 'Profile image it\'s too large' )]
-    public $customer_photo;
 
     #[Validate(['required', 'string', 'max:255', 'min:5'])]
     public string $full_name = '';
