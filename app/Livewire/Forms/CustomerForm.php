1ca96509017b6ff468bb601d00ae49d91fb11cf2
danilo.quattrini <daniloquattrini.com@gmail.com>
2025-12-15 11:03:30 +0100

    fix(customer-create): fix customer validation email

--- app/Livewire/Forms/CustomerForm.php
+++ app/Livewire/Forms/CustomerForm.php
@@ -16,7 +16,7 @@ class CustomerForm extends Form
     #[Validate(['required', 'string', 'max:255', 'min:5'])]
     public string $full_name = '';
 
-    #[Validate(['required', 'string', 'email', 'max:255', 'unique:users'])]
+    #[Validate(['required', 'string', 'email', 'max:255', 'unique:customers,users'])]
     public string $email = '';
 
     #[Validate('required')]
