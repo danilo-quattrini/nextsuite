8eadfd7b3a581a16a9f27a4828023ba49661f932
danilo.quattrini <daniloquattrini.com@gmail.com>
2025-12-16 09:49:06 +0100

    fix(!select-wrapper-element): change variable from const to let due to alpine js error, remove comment.

--- resources/views/components/form/select-element.blade.php
+++ resources/views/components/form/select-element.blade.php
@@ -7,14 +7,14 @@
         name="{{ $name }}"
         id="{{ $id ?? $name }}"
         {{ $attributes->merge(['class' => 'select absolute inset-0 w-full h-full cursor-pointer opacity-0']) }}
-        wire:model.defer="field"
+        wire:model.defer="{{$model}}"
         x-ref="select"
         x-on:change="
-        const selected = $refs.select.selectedOptions[0];
+        let selected = $refs.select.selectedOptions[0];
         $dispatch('select-changed', { label: selected ? selected.text : 'Select an option' });
     "
         x-on:input="
-        const selected = $refs.select.selectedOptions[0];
+        let selected = $refs.select.selectedOptions[0];
         $dispatch('select-changed', { label: selected ? selected.text : 'Select an option' });
     "
 >
@@ -29,18 +29,11 @@
 </select>
 
 <span
-        class="pointer-events-none font-medium"
+        class="pointer-events-none font-medium text-black"
         x-data="{ label: 'Select an option' }"
         x-init="
-        // set label from the select on init (works if a value is pre-selected)
-        const selected = $refs.select?.selectedOptions?.[0];
+        let selected = $refs.select?.selectedOptions?.[0];
         label = selected && selected.text ? selected.text : 'Select an option';
-    "
-        x-on:change.window="
-        if ($refs.select) {
-            const selected = $refs.select.selectedOptions[0];
-            label = selected ? selected.text : 'Select an option';
-        }
     "
         x-on:select-changed.window="
         label = $event.detail.label;
