522099909434067b7b39170b5d98668e68378337
danilo.quattrini <daniloquattrini.com@gmail.com>
2025-12-17 15:19:35 +0100

    feat(customer-factory): update CustomerFactory.php to generate real data.

--- database/factories/CustomerFactory.php
+++ database/factories/CustomerFactory.php
@@ -2,7 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\Company;
 use App\Models\Customer;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Carbon;
 
@@ -13,12 +15,15 @@ class CustomerFactory extends Factory
     public function definition(): array
     {
         return [
+            'profile_photo_url' => $this->faker->name(),
             'full_name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'phone' => $this->faker->phoneNumber(),
             'dob' => Carbon::now(),
             'nationality' => $this->faker->country(),
             'gender' => $this->faker->word(),
+            'company_id' => Company::factory(),
+            'user_id' => User::factory(),
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
         ];
