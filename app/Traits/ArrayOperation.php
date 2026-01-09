c485af1a9e477528ee0b4905953b0d2441fabd67
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2026-01-09 12:59:11 +0100

    feat(array-operation): add new array operation inside the ArrayOperation.php trait.

--- app/Traits/ArrayOperation.php
+++ app/Traits/ArrayOperation.php
@@ -5,7 +5,54 @@ namespace App\Traits;
 trait ArrayOperation
 {
     /**
-     * Remove an item from a form array property by its key.
+     * Add an item from a form array property by its key from a form.
+     *
+     * @param string $targetProperty The name of the array that we want to add an obj.
+     * @param int|string $key The key of the item to remove.
+     * @param  string  $sourceProperty  the name of the item to add.
+     * @param  string  $sourceKey  the key of the item to add.
+     */
+    public function toggleItem(
+        string $targetProperty,
+        int|string $key,
+        string $sourceProperty,
+        string $sourceKey = 'id'
+    ): void {
+        if (!isset($this->{$targetProperty}) || !is_array($this->{$targetProperty})) {
+            $this->{$targetProperty} = [];
+        }
+
+        if (isset($this->{$targetProperty}[$key])) {
+            unset($this->{$targetProperty}[$key]);
+            return;
+        }
+
+        if (!isset($this->{$sourceProperty})) {
+            return;
+        }
+
+        $item = $this->{$sourceProperty}->firstWhere($sourceKey, $key);
+
+        if ($item) {
+            $this->{$targetProperty}[$key] = $item;
+        }
+    }
+
+    /**
+     * Check if an item is inside the array though its key and remove it.
+     *
+     * @param array $array The array to check and remove the item from it.
+     * @param int|string $key The key of the item to remove.
+     */
+    protected function removeFromArray(array &$array, int|string $key): void
+    {
+        if (array_key_exists($key, $array)) {
+            unset($array[$key]);
+        }
+    }
+
+    /**
+     * Remove an item from a form array property by its key from a form.
      *
      * @param string $property The name of the array property on the form object.
      * @param int|string $key The key of the item to remove.
@@ -13,9 +60,20 @@ trait ArrayOperation
     public function removeItem(string $property, int|string $key): void
     {
         if (isset($this->form->{$property}) && is_array($this->form->{$property})) {
-            if (array_key_exists($key, $this->form->{$property})) {
-                unset($this->form->{$property}[$key]);
-            }
+            $this->removeFromArray($this->form->{$property}, $key);
+        }
+    }
+
+    /**
+     * Remove an item from a form array property by its key.
+     *
+     * @param string $property The name of the array property on the form object.
+     * @param int|string $key The key of the item to remove.
+     */
+    public function removeArrayItem(string $property, int|string $key): void
+    {
+        if (isset($this->{$property}) && is_array($this->{$property})) {
+            $this->removeFromArray($this->{$property}, $key);
         }
     }
 
