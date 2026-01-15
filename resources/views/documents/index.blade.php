cd1043cabbcb5c2546a55aba10a5de929e85f0f6
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2026-01-15 12:34:55 +0100

    refactor(!!customer-document): now only the customer who owns the document it will show the dropdown menu.

--- resources/views/documents/index.blade.php
+++ resources/views/documents/index.blade.php
@@ -40,37 +40,40 @@
                         </div>
                     </div>
 
+                    {{--    DOCUMENT OPERATIONS --}}
                     <div class="mt-6 flex gap-3">
+                        @if($customer->documents()->exists())
+                            <x-form.dropdown-button>
+                                <x-slot:trigger>
+                                    <x-button size="auto" variant="rest">
+                                        <x-heroicon name="document-magnifying-glass"/>
+                                        Documents
+                                        <x-heroicon
+                                                name="chevron-down"
+                                                size="md"
+                                                class="transition-transform"
+                                                x-bind:class="open ? 'rotate-180' : ''"
+                                        />
+                                    </x-button>
+                                </x-slot:trigger>
 
-                        <x-form.dropdown-button>
-                            <x-slot:trigger>
-                                <x-button size="auto" variant="rest">
-                                    <x-heroicon name="document-magnifying-glass"/>
-                                    Documents
-                                    <x-heroicon
-                                            name="chevron-down"
-                                            size="md"
-                                            class="transition-transform"
-                                            x-bind:class="open ? 'rotate-180' : ''"
-                                    />
-                                </x-button>
-                            </x-slot:trigger>
+                                <x-slot:content>
+                                    <div class="flex flex-col gap-2">
 
-                            <x-slot:content>
-                                <div class="flex flex-col gap-2">
+                                            @foreach($customer->documents as $document)
+                                                <a href="{{ route('document.show', $document->id) }}">
+                                                    <div class="flex px-4 py-2 rounded-md text-sm font-medium border border-outline-grey hover:bg-outline-grey">
+                                                        <x-heroicon name="document-text"/>
+                                                        {{ ucfirst($document->type) . ' ' . ' ' . $customer->full_name ?? 'Document' }}
+                                                    </div>
+                                                </a>
+                                            @endforeach
 
-                                    @foreach($customer->documents as $document)
-                                        <a href="{{ route('document.show', $document->id) }}">
-                                            <div class="flex px-4 py-2 rounded-md text-sm font-medium border border-outline-grey hover:bg-outline-grey">
-                                                <x-heroicon name="document-text"/>
-                                                {{ ucfirst($document->type) . ' ' . ' ' . $customer->full_name ?? 'Document' }}
-                                            </div>
-                                        </a>
-                                    @endforeach
-                                </div>
-                            </x-slot:content>
-                        </x-form.dropdown-button>
 
+                                    </div>
+                                </x-slot:content>
+                            </x-form.dropdown-button>
+                        @endif
                         <x-button
                                 onclick="{{$showDocumentGenerationModal = false}}"
                                 href="{{ route('document.create', ['customer' => $customer->id]) }}"
@@ -80,6 +83,7 @@
                             New Document
                         </x-button>
                     </div>
+
                 </div>
             @endforeach
         </div>
