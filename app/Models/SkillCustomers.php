a3d1ae52ac01bf743e28a31c53e3b281de0deb93
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2026-02-02 11:14:22 +0100

    feat(skill-customer-model): add new pivot table model for customer and skill models.

---
+++ app/Models/SkillCustomers.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+
+
+use Illuminate\Database\Eloquent\Relations\Pivot;
+
+class SkillCustomers extends Pivot
+{
+    protected $table = 'skill_customers';
+
+    public $incrementing = true;
+
+    protected $fillable = [
+        'level',
+        'years',
+        'notes',
+        'skill_id',
+        'customer_id',
+        'created_at',
+        'updated_at'
+    ];
+
+}
