583851756af7ae57d4cedd0822b7a74e49716eec
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2026-02-03 09:41:18 +0100

    feat(skill-evaluation-model): add new model for the skill evaluation table.

--- app/Models/SkillEvaluations.php
+++ app/Models/SkillEvaluations.php
@@ -3,14 +3,36 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class SkillEvaluations extends Model
 {
     protected $fillable = [
+        'skill_customer_id',
+        'evaluator_id',
         'level',
         'notes',
-        'skill_customer_id',
-        'evaluator_id'
+        'evaluated_at'
     ];
 
+    protected $casts = [
+        'evaluated_at' => 'datetime'
+    ];
+
+    /**
+     * Retrive all the evaluators from the SkillEvaluation model
+     */
+    public function evaluator(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'evaluator_id');
+    }
+
+    /**
+     * Retrive all the skills from the SkillCustomer  model
+     */
+
+    public function skillCustomer(): BelongsTo
+    {
+        return $this->belongsTo(SkillCustomers::class, 'skill_customer_id');
+    }
 }
