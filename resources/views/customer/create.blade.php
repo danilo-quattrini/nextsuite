6ba414d6fd73277027a5246257c9c9adf54f210c
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2026-02-06 17:41:13 +0100

    refactor: change create customer layout.

--- resources/views/customer/create.blade.php
+++ resources/views/customer/create.blade.php
@@ -1,22 +1,29 @@
 <x-app-layout>
-    <div class="flex w-full mx-auto justify-center items-center">
+    <div class="page-content__container">
 
         {{--  Content  --}}
-        <div class="flex-col space-y-6">
+        <div class="page-content__hero">
+            <div class="page-content__hero-inner">
+                <div class="page-content__hero-row">
+                    <div class="page-content__hero-copy">
+                        <h2 class="page-content__title">
+                            {{ __('Create Customer') }}
+                        </h2>
+                        <p class="page-content__subtitle">
+                            {{__('Complete the details in two steps.')}}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-            {{--  Page Header  --}}
-            <h1>
-                {{ __('Create Customer') }}
-            </h1>
-
-            <p class="text-sm text-primary-grey">
-                {{__('Complete the details in two steps.')}}
-            </p>
-
+        <div class="page-content__body">
             {{--  Form  --}}
-            <x-form.container>
-                @livewire('customer.create-customer')
-            </x-form.container>
+            <div class="page-content__card">
+                <x-form.container>
+                    @livewire('customer.create-customer')
+                </x-form.container>
+            </div>
         </div>
 
     </div>
