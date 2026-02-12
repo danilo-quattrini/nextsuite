5f2fca48850880b39314953c80d4d50ca027a15f
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2026-02-12 08:44:11 +0100

    refactor: separate the logic of each piece of code inside SkillSeeder.php, in this case it will not be complicated to read it.

--- database/seeders/SkillSeeder.php
+++ database/seeders/SkillSeeder.php
@@ -2,52 +2,103 @@
 
 namespace Database\Seeders;
 
+use App\Enums\CategoryType;
 use App\Models\Category;
 use App\Models\Skill;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\File;
 
 class SkillSeeder extends Seeder
 {
+    private const string SKILL_JSON_PATH = 'data/skill/skills.json';
+    private const string SOFT_SKILL_TYPE = 'soft_skill';
+
     public function run(): void
     {
-        $skillsByCategory = json_decode(
-            file_get_contents(database_path('data/skill/skills.json')),
-            true
-        );
-
+        $skillsByCategory = $this->loadSkillsData();
         foreach ($skillsByCategory as $categoryName => $skills) {
             $category = Category::where('name', $categoryName)->first();
-
-
             if (! $category) {
                 continue;
             }
+            $this->processSkills($skills, $category);
+        }
+    }
 
-            foreach ($skills as $skillData) {
+    private function shouldActivateSkill(CategoryType $categoryType): bool
+    {
+        return $categoryType->value !== self::SOFT_SKILL_TYPE;
+    }
 
-                if(isset($skillData['subcategory'])){
-                    $subcategory = Category::firstOrCreate([
-                        'name' => $skillData['subcategory'],
-                        'type' => $skillData['type'],
-                    ]);
+    private function hasSubcategory(array $skillData): bool
+    {
+        return isset($skillData['subcategory']);
+    }
 
-                    foreach ($skillData['skills'] as $subCategorySkills){
-                        Skill::firstOrCreate([
-                            'name' => $subCategorySkills['name'],
-                            'category_id' => $subcategory->id,
-                        ], [
-                            'description' => $subCategorySkills['description'] ?? null,
-                        ]);
-                    }
-                }else{
-                    Skill::firstOrCreate([
-                        'name' => $skillData['name'],
-                        'category_id' => $category->id,
-                    ], [
-                        'description' => $skillData['description'] ?? null,
-                    ]);
-                }
+    private function createSkill(
+        string $name,
+        int $categoryId,
+        ?string $description,
+        bool $state
+    ): void
+    {
+        Skill::firstOrCreate([
+            'name' => $name,
+            'category_id' => $categoryId,
+        ], [
+            'description' => $description ?? null,
+            'state' => $state
+        ]);
+    }
+
+    /**
+     * @return mixed
+     */
+    private function loadSkillsData(): array
+    {
+        return json_decode(
+            file_get_contents(database_path(self::SKILL_JSON_PATH)),
+            true
+        );
+    }
+
+    /**
+     * @param  mixed  $skillData
+     * @return void
+     */
+    private function handleSubCategory(mixed $skillData): void
+    {
+        $subcategory = Category::firstOrCreate([
+            'name' => $skillData['subcategory'],
+            'type' => $skillData['type'],
+        ]);
+
+        foreach ($skillData['skills'] as $subCategorySkills) {
+            $this->createSkill(
+                $subCategorySkills['name'],
+                $subcategory->id,
+                $subCategorySkills['description'] ?? null,
+                $this->shouldActivateSkill($subcategory->type)
+            );
+        }
+    }
+
+    /**
+     * @param  mixed  $skills
+     * @param  Category  $category
+     * @return void
+     */
+    private function processSkills(mixed $skills, Category $category): void
+    {
+        foreach ($skills as $skillData) {
+            if ($this->hasSubcategory($skillData)) {
+                $this->handleSubCategory($skillData);
+            } else {
+                $this->createSkill(
+                    $skillData['name'],
+                    $category->id,
+                    $skillData['description'] ?? null,
+                    $this->shouldActivateSkill($category->type)
+                );
             }
         }
     }
