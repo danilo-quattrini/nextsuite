1f57432e1dbe84143dc49d4ecae7c8c5db1155aa
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2026-02-12 18:44:34 +0100

    feat: add empty state blade component with its style.

---
+++ resources/views/components/empty-state.blade.php
@@ -0,0 +1,9 @@
+@props(['icon' => 'inbox', 'message', 'description' => null])
+
+<div class="empty-state">
+    <x-heroicon :name="$icon" variant="outline" class="empty-state__icon"/>
+    <p class="empty-state__message">{{ $message }}</p>
+    @if($description)
+        <p class="empty-state__description">{{ $description }}</p>
+    @endif
+</div>
