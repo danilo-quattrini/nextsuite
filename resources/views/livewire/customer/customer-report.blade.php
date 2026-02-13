026622f9793e58385277727a03598c84cb3ddc66
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2026-02-13 16:39:06 +0100

    refactor: adapt the customer-report.blade.php and CustomerReport.php component to the new component content-page-card.blade.

--- resources/views/livewire/customer/customer-report.blade.php
+++ resources/views/livewire/customer/customer-report.blade.php
@@ -1,27 +1,21 @@
-<div class="page-content__container">
-    <div class="page-content__hero">
-        <div class="page-content__hero-inner">
-            <div class="page-content__hero-row">
-                <div class="page-content__hero-copy">
-                    <h2 class="page-content__title">
-                        {{ __('Reports') }}
-                    </h2>
-                    <p class="page-content__subtitle">
-                        {{__('Select a customer that you want to see its report')}}
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="page-content__body">
-        <div class="page-content__grid">
-            @foreach($customers as $customer)
-                <x-user-card :user="$customer" href="report.show"/>
-            @endforeach
-        </div>
+<x-card.content-page-card
+        title="Reports"
+        description="Select a customer that you want to see its report"
+        :has-counter="true"
+        :has-grid="true"
+        :counter-title="Str::plural('Customer', count($this->customers) ?? 0)"
+        :counter-value="$this->customers->total()"
+>
+    @foreach($this->customers as $customer)
+        <x-user-card :user="$customer" href="report.show"/>
+    @endforeach
 
-        <div class="page-content__pagination">
-            {{ $customers->links('components.pagination') }}
-        </div>
-    </div>
-</div>
+    @if($this->customers->total() > 6)
+        <x-slot:pagination>
+            <div class="page-content__pagination">
+                {{ $this->customers->links('components.pagination') }}
+            </div>
+        </x-slot:pagination>
+    @endif
+
+</x-card.content-page-card>
