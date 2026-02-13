e0cce2a1b5ed14bf5e8c996e245fa4a32c39ba8e
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2026-02-13 09:40:02 +0100

    feat: add sizable title for the card container component.

--- resources/views/components/card/card-container.blade.php
+++ resources/views/components/card/card-container.blade.php
@@ -1,9 +1,24 @@
 @props([
-    'title' => 'Title'
+    'title' => 'Title',
+    'size' => null
 ])
+@php
+    $sizes = [
+        'sm' => ' text-sm ',
+        'md' => ' text-md ',
+        'lg' => ' text-base ',
+        'xl' => ' text-xl ',
+        '2xl' => ' text-2xl ',
+        '3xl' => ' text-3xl '
+    ]
+@endphp
 <div class="card-container">
     <div class="card-head">
-        <h3>{{ $title }}</h3>
+        @if('size')
+            <span {{ $attributes->merge(['class' => $sizes[$size] ?? $sizes['3xl'] ] ) }}> {{ $title }} </span>
+        @else
+            <h3>{{ $title }}</h3>
+        @endif
         {{ $action ?? null }}
     </div>
     {{ $slot }}
