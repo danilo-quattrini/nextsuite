0bf77e533e14f1092b543b17c28d30c316f3b8ae
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2026-02-15 18:46:05 +0100

    refactor: convert sfc (single file component) into a multi-flile component.

---
+++ resources/views/pages/skill/schema/create/create.blade.php
@@ -0,0 +1,74 @@
+@php use Carbon\Carbon; @endphp
+<x-card.content-page-card
+        title="Create Skill"
+        description="Choose the skills you want to define to the customer."
+        :has-counter="false"
+        :has-grid="false"
+>
+    <div class="page-content__card">
+        <div class="user-view">
+            <livewire:user.info-header :user="$customer"/>
+            <form wire:submit.prevent="create" enctype="multipart/form-data">
+                @csrf
+                <div class="grid lg:grid-cols-4 md:grid-cols-2 items-center gap-6">
+                    {{-- CATEGORIES CARD --}}
+                    @foreach($categories as $category)
+                        <x-card.card-container size="2xl" :title="$category['name']">
+                            <x-slot:action>
+                                <x-form.toggle-switch
+                                        id="category-{{ $category['id'] }}"
+                                        name="category-{{ $category['name'] }}"
+                                        value="{{ $category['id'] }}"
+                                        wire:model.live="selectedCategory"
+                                />
+                            </x-slot:action>
+
+                            {{-- SKILL SELECTION (Dependent on Category) --}}
+                            @php
+                                $isCategorySelected = $this->isCategorySelected($category['id']);
+                            @endphp
+
+                            @foreach($category['skills'] as $skill)
+                                @php
+                                    $isSkillSelected = $this->isSkillSelected($skill['id']);
+                                @endphp
+                                <div class="flex flex-wrap gap-6 {{ !$isCategorySelected ? 'opacity-50 pointer-events-none' : '' }}"
+                                     wire:transition>
+                                    @if($isSkillSelected)
+                                        <x-form.slider
+                                                id="skill-{{$skill['value']}}-level"
+                                                name="skill-{{$skill['value']}}-level"
+                                                wire:model.live="skillDefaultLevel.{{ $skill['id'] }}"
+                                        />
+                                    @endif
+                                    <x-toggle-container wire:key="{{ $skill['id'] }}">
+                                        <x-slot:element>
+                                            <x-form.checkbox
+                                                    id="skill-{{ $skill['id'] }}"
+                                                    name="skill-{{ $skill['label'] }}"
+                                                    value="{{ $skill['id'] }}"
+                                                    wire:model.live="selectedSkills"
+                                                    :disabled="!$isCategorySelected"
+                                            />
+                                        </x-slot:element>
+
+                                        <x-slot:span>
+                                            <span class="ds-checkbox-mark"></span>
+                                        </x-slot:span>
+                                        {{ $skill['label'] }}
+                                    </x-toggle-container>
+                                </div>
+                            @endforeach
+                        </x-card.card-container>
+                    @endforeach
+                </div>
+
+                <div class="flex lg:justify-end md:justify-center">
+                    <x-button size="large" type="submit">
+                        Create
+                    </x-button>
+                </div>
+            </form>
+        </div>
+    </div>
+</x-card.content-page-card>
---
+++ resources/views/pages/skill/schema/create/create.php
@@ -0,0 +1,80 @@
+<?php
+
+use App\Domain\Skill\Services\SkillSchemaService;
+use App\Models\Customer;
+use Livewire\Attributes\Computed;
+use Livewire\Component;
+use App\Models\Category;
+use App\Models\Skill;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Auth;
+
+new class extends Component {
+    public ?Customer $customer = null;
+    public ?Collection $categories = null;
+
+    public ?array $selectedCategory = [];
+    public ?array $selectedSkills = [];
+    public ?array $skillDefaultLevel = [];
+
+    public SkillSchemaService $schemaService;
+
+    #[Computed]
+    public function loadService(SkillSchemaService $schemaService): void
+    {
+        $this->schemaService = $schemaService;
+    }
+
+    public function mount(): void
+    {
+        $this->categories = $this->computeCategories();
+    }
+
+    public function create(): void
+    {
+        // TODO: ADD VALIDATION
+        $user = Auth::user();
+
+        $user->skillSchema()->syncWithoutDetaching($this->selectedSkill);
+    }
+
+
+    public function isCategorySelected($categoryId): bool
+    {
+        return in_array($categoryId, $this->selectedCategory);
+    }
+
+    public function isSkillSelected($skillId): bool
+    {
+        return in_array($skillId, $this->selectedSkills);
+    }
+
+    #[Computed]
+    public function computeCategories(): Collection
+    {
+        return Category::where('type', 'soft_skill')
+            ->where('name', '<>', 'Abilities')
+            ->get()
+            ->map(function ($category) {
+                return [
+                    'id' => $category->id,
+                    'name' => $category->name,
+                    'skills' => $this->computeSkills($category->id)
+                ];
+            });
+    }
+
+    public function computeSkills($categoryId): array
+    {
+        return Skill::where('category_id', $categoryId)
+            ->get()
+            ->map(function ($skill) {
+                return [
+                    'id' => $skill->id,
+                    'label' => $skill->name,
+                    'value' => strtolower(str_replace(" ", "-", $skill->name))
+                ];
+            })->toArray();
+
+    }
+};
