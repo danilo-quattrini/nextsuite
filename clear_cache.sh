1faf1caf29fc664a2ec0907b77f713de6f81fa14
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2026-02-16 09:18:14 +0100

    feat: bash script to execute in production when deploying the web app.

---
+++ clear_cache.sh
@@ -0,0 +1,23 @@
+#!/bin/zsh
+
+# Clear everything
+php artisan optimize:clear
+
+# Or clear individually
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
+php artisan cache:clear
+
+echo "Cleaned the cache!"
+
+# Rebuild caches
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+echo "Rebuild the caches!"
+
+# Verify cache was rebuilt
+ls -la bootstrap/cache/
+echo "Check if the cash as been rebuilt!"
