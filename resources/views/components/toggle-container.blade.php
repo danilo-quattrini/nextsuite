b2ae4364211680dee3f17598bfb0173a72612869
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2026-02-16 11:24:54 +0100

    feat: add description in the checkbox component.

--- resources/views/components/toggle-container.blade.php
+++ resources/views/components/toggle-container.blade.php
@@ -1,5 +1,15 @@
+@props([
+    'description' => null
+])
 <label class="ds-checkbox-container">
     {{$element}}
     {{$span}}
-    <span class="ds-checkbox-label">{{$slot}}</span>
+    <span class="ds-checkbox-label">
+        {{$slot}}
+        <br>
+        @if($description)
+            <span class="ds-checkbox-description">{{$description}}</span>
+        @endif
+    </span>
+
 </label>
