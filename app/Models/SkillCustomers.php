b77af6e0f5efa19c5389d62fc6da208aec13e493
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2026-02-16 12:27:14 +0100

    feat: implement interface for dedicate SkillAssignable interface.

--- app/Models/SkillCustomers.php
+++ app/Models/SkillCustomers.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 
 
+use App\Domain\Skill\Contracts\SkillAssignable;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\Pivot;
