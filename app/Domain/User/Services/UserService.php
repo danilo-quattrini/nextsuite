5074f24046c1e4ca41f8003ab7ae509f81ad1ed0
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2026-02-20 17:46:41 +0100

    feat(user-service): new service class to perform all user operation.

---
+++ app/Domain/User/Services/UserService.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Domain\User\Services;
+
+use App\Services\OpenAIService;
+use Illuminate\Database\Eloquent\Model;
+
+class UserService
+{
+    public function __construct(
+        private readonly ?Model $user = null
+    ) {}
+
+    /**
+     * Get the review made from the OpenAI service in base of user info
+     * */
+    public function getUserReview(): string
+    {
+
+        if($this->isNotEmpty()) {
+            $openAIService = new OpenAIService();
+            return $openAIService->generateReview($this->getUser()?->full_name);
+        }else{
+            return 'No, customer has been assigned!';
+        }
+    }
+
+    /**
+     * Get the User model
+     * */
+    public function getUser(): ?Model
+    {
+        return $this->user;
+    }
+
+    /**
+     * Check if the user it's not empty
+     * */
+    private function isNotEmpty(): bool
+    {
+        return !empty($this->user);
+    }
+
+    /**
+     * Check if the user relation with skill exists
+     * */
+    private function hasSkill(): bool
+    {
+        return $this->user->skills()->exists();
+    }
+
+}
