659d56e056e431a66746717726db9eda253c22e8
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2026-02-23 11:44:21 +0100

    refactor(report-user-summary): change user summary to match the user need with more clean and visible part.

--- resources/views/components/card/user-summary-card.blade.php
+++ resources/views/components/card/user-summary-card.blade.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Domain\Skill\Contracts\SkillAssignable;
 use App\Domain\Skill\Services\SkillService;
 use App\Domain\User\Services\UserService;
 use Illuminate\Database\Eloquent\Model;
@@ -7,16 +8,19 @@ use Illuminate\Support\Collection;
 use Livewire\Attributes\Lazy;
 use Livewire\Component;
 
-new #[Lazy] class extends Component {
+new #[Lazy]
+class extends Component {
 
-    public ?Model $user = null;
+    public ?SkillAssignable $user = null;
     public ?string $summary = '';
     public ?Collection $hardSkills = null;
 
     public function mount(): void
     {
-        $service = new UserService($this->user);
-        $this->summary = $service->getUserReview();
+        if($this->user->hasSkill()) {
+            $service = new UserService($this->user);
+            $this->summary = $service->getUserReview();
+        }
     }
 
 
@@ -24,30 +28,32 @@ new #[Lazy] class extends Component {
     public function placeholder(): string
     {
         return <<<'HTML'
-            <div class="user-view__panel user-view__panel--wide">
-                <div class="user-view__panel-header">
-                    <div class="user-view__panel-header--left">
-                        <h3>Summary</h3>
-                        <p class="user-view__panel-subtitle">That's a quick review about the customer.</p>
+                <x-card.card-container
+            title="Summary"
+            subtitle="That's a quick review about the customer"
+                    >
                         <div class="summary-skeleton">
                             <div class="skeleton-line"></div>
                             <div class="skeleton-line skeleton-line--short"></div>
                             <div class="skeleton-line"></div>
                         </div>
-                    </div>
-                </div>
-            </div>
+               </x-card.card-container>
         HTML;
     }
 };
 ?>
 
-<div class="user-view__panel user-view__panel--wide">
-    <div class="user-view__panel-header">
-        <div class="user-view__panel-header--left">
-            <h3>Summary</h3>
-            <p class="user-view__panel-subtitle">That's a quick review about the customer.</p>
-            <p>{{ $summary }}</p>
-        </div>
-    </div>
-</div>
+<x-card.card-container
+        title="Summary"
+        subtitle="That's a quick review about the customer"
+>
+    @if($this->user->hasSkill())
+        <p>{{ $summary }}</p>
+    @else
+        <x-empty-state
+                icon="cube-transparent"
+                message="This customer has no skill added yet"
+                description="Add a skill or schema to this customer to get an evaluation"
+        />
+    @endif
+</x-card.card-container>
