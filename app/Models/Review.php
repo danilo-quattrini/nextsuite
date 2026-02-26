2a457d9633cec85a2ce6eb22755ff4e091e97bb5
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2026-02-26 11:44:34 +0100

    feat: define helper methods in the Review model.

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -27,5 +26,31 @@ class Review extends Model
     public function reviewable(): MorphTo
     {
         return $this->morphTo();
+    }
+
+    // ==================== HELPER METHODS ====================
+
+    /**
+     * Get the reviewable model class
+     */
+    public function getModelClass(): Model|string
+    {
+        return $this->reviewable_type;
+    }
+
+    /**
+     * Get the reviewable model name
+     */
+    public function getModelName(): string
+    {
+        return class_basename($this->reviewable_type);
+    }
+
+    /**
+     * Get the reviewable model class
+     */
+    public function getModelId(): int
+    {
+        return $this->reviewable_id;
     }
 }
