f6482222974bc8d9f5221842029aba5ec2267c10
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2026-02-26 12:54:31 +0100

    feat: add cache management to Review model
    - Implemented cache clearing methods (`clearReviewableCache`) for reviews.
    - Utilized cache tags to manage cache efficiently on review creation, update, and deletion.

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Cache;
 
 class Review extends Model
 {
@@ -16,7 +17,39 @@ class Review extends Model
         'rating',
         'comment',
         'author_id',
+        'reviewable_type',
+        'reviewable_id',
     ];
+
+    protected static function booted(): void
+    {
+        $clearReviewableCache = function($review) {
+            $review->clearReviewableCache();
+        };
+
+        static::created($clearReviewableCache);
+        static::updated($clearReviewableCache);
+        static::deleted($clearReviewableCache);
+    }
+
+    // ==================== CACHE OPERATION ====================
+
+    public function clearReviewableCache(): void
+    {
+        if(!$this->reviewable_id || !$this->reviewable_type) {
+            return;
+        }
+
+        $model = $this->getModelClass();
+
+        if(method_exists($model, 'clearModelCache' )){
+            $model::clearModelCache($this->getModelId());
+        }
+
+        Cache::tags([strtolower($this->getModelName())])->flush();
+    }
+
+    // ==================== RELATIONSHIPS ====================
 
     public function author(): BelongsTo
     {
