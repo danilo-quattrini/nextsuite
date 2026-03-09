c0719be812d5f8a4750fc0272bd9afab4b9f6820
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2026-03-09 23:51:57 +0100

    feat: implement first table header.

--- resources/views/components/data-table.blade.php
+++ resources/views/components/data-table.blade.php
@@ -1,3 +1,25 @@
 <div>
-
+    <div class="hidden md:block table-container">
+        <table class="min-w-full w-full leading-normal">
+            <thead>
+                <tr>
+                    @foreach($columns as $column)
+                        @if($isColumnVisible($column))
+                            <th class="table-head__container {{ $isHiddenOnMobile($column) ? 'hidden md:table-cell' : '' }}">
+                                <div class="table-head__content">
+                                    @if(isset($column['icon']))
+                                        <x-heroicon
+                                                :name="$column['icon']"
+                                                size="lg"
+                                        />
+                                    @endif
+                                    <span>{{ $column['label'] }}</span>
+                                </div>
+                            </th>
+                        @endif
+                    @endforeach
+                </tr>
+            </thead>
+        </table>
+    </div>
 </div>
