51a42dff983c3f6e700c51d6a33664f1bbc8d884
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2026-03-13 14:54:20 +0100

    feat: new migration for table role_field, where each role it's associated with a field

---
+++ database/migrations/2026_03_12_114811_create_role_field.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Models\Field;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Spatie\Permission\Models\Role;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('role_field', function (Blueprint $table) {
+            $table->foreignIdFor(Role::class)->constrained('roles')->cascadeOnDelete();
+            $table->foreignIdFor(Field::class)->constrained('fields')->cascadeOnDelete();
+
+            $table->timestamps();
+
+            $table->unique(['role_id', 'field_id']);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('role_field');
+    }
+};
