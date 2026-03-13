22b57818561e8191b0a29caf8894a45c783ae36c
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2026-03-13 14:55:30 +0100

    feat: add new class to use for associate the Role to each field.

---
+++ database/seeders/RoleFieldSeeder.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Field;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class RoleFieldSeeder extends Seeder
+{
+    public function run(): void
+    {
+    }
+}
