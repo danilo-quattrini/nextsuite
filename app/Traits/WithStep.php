33534c0a2ac66c5580a31ca98a160b313ce0ed5e
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2026-03-19 12:04:12 +0100

    docs(comments): explained methods nextStep and stepRules.

--- app/Traits/WithStep.php
+++ app/Traits/WithStep.php
@@ -6,13 +6,30 @@ trait WithStep
 {
 
     public int $step = 1;
+
+    /**
+     * Abstract Method where we define an array of
+     * rule, the key of the array it's the attribute to
+     * validate, the values are an array or a
+     * string of rule, defined for the key.
+     *
+     * @return array of rules;
+     * */
     abstract protected function stepRules(): array;
 
+    /**
+     * Method that retrive the array of
+     * rules from the output of the method stepRules().
+     * It will check if the array has been created or not,
+     * then it will get all the values from the arrays' keys.
+     * Check if the value has the property form or not, and it will
+     * add it if it exists and validate it.
+    */
     public function nextStep(): void
     {
         $rules = $this->stepRules()[$this->step] ?? null;
 
-        if ($rules) {
+        if (!empty($rules)) {
             $resolvedRules = value($rules);
 
             if (!empty($resolvedRules)) {
