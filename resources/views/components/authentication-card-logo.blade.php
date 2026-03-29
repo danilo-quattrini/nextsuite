7974ed396c58ba147bbdf12e01f77972114d3a51
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2026-03-29 01:12:42 +0100

    refactor (autentication-card-logo): change logo to be uniform and sizable.

--- resources/views/components/authentication-card-logo.blade.php
+++ resources/views/components/authentication-card-logo.blade.php
@@ -1,5 +1,22 @@
-<a href="/" class="self-stretch inline-flex justify-start items-start gap-4">
-    <svg class="size-16" viewBox="0 0 63 63" fill="none" xmlns="http://www.w3.org/2000/svg">
+@props([
+    'showName' => true,
+    'size'     => 'md',
+    'href'     => '/',
+])
+
+@php
+    $sizes = [
+        'sm' => ['icon' => 'size-8',  'text' => 'text-xl'],
+        'md' => ['icon' => 'size-12', 'text' => 'text-4xl'],
+        'lg' => ['icon' => 'size-16', 'text' => 'text-5xl'],
+    ];
+    $s = $sizes[$size] ?? $sizes['md'];
+@endphp
+
+<a href="{{ $href }}" class="inline-flex items-center gap-3 group">
+
+    {{-- Icon --}}
+    <svg class="{{ $s['icon'] }} shrink-0" viewBox="0 0 63 63" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M0 16.3281C0 7.31034 7.31034 0 16.3281 0H45.7187C54.7365 0 62.0468 7.31034 62.0468 16.3281V45.7187C62.0468 54.7365 54.7365 62.0468 45.7187 62.0468H16.3281C7.31034 62.0468 0 54.7365 0 45.7187V16.3281Z" fill="#5E81F4"/>
         <g filter="url(#filter0_d_43_771)">
             <path d="M16.3285 16.3287C16.3285 14.5252 17.7906 13.0631 19.5941 13.0631C21.3977 13.0631 22.8597 14.5252 22.8597 16.3287V45.7193C22.8597 47.5229 21.3977 48.9849 19.5941 48.9849C17.7906 48.9849 16.3285 47.5229 16.3285 45.7193V16.3287Z" fill="white"/>
@@ -14,8 +31,7 @@
             <filter id="filter0_d_43_771" x="15.3285" y="12.5631" width="8.53125" height="37.9218" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
                 <feFlood flood-opacity="0" result="BackgroundImageFix"/>
                 <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-                <feOffset dy="0.5"/>
-                <feGaussianBlur stdDeviation="0.5"/>
+                <feOffset dy="0.5"/><feGaussianBlur stdDeviation="0.5"/>
                 <feComposite in2="hardAlpha" operator="out"/>
                 <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.12 0"/>
                 <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_43_771"/>
@@ -24,8 +40,7 @@
             <filter id="filter1_d_43_771" x="27.5743" y="25.6248" width="8.53125" height="24.8593" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
                 <feFlood flood-opacity="0" result="BackgroundImageFix"/>
                 <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-                <feOffset dy="0.5"/>
-                <feGaussianBlur stdDeviation="0.5"/>
+                <feOffset dy="0.5"/><feGaussianBlur stdDeviation="0.5"/>
                 <feComposite in2="hardAlpha" operator="out"/>
                 <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.12 0"/>
                 <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_43_771"/>
@@ -34,8 +49,7 @@
             <filter id="filter2_d_43_771" x="39.8197" y="17.4603" width="8.53125" height="33.0234" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
                 <feFlood flood-opacity="0" result="BackgroundImageFix"/>
                 <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-                <feOffset dy="0.5"/>
-                <feGaussianBlur stdDeviation="0.5"/>
+                <feOffset dy="0.5"/><feGaussianBlur stdDeviation="0.5"/>
                 <feComposite in2="hardAlpha" operator="out"/>
                 <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.12 0"/>
                 <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_43_771"/>
@@ -43,5 +57,12 @@
             </filter>
         </defs>
     </svg>
-    <h1>NextSuite</h1>
+
+    {{-- Wordmark --}}
+    @if ($showName)
+        <span class="{{ $s['text'] }} font-semibold tracking-tight">
+            NextSuite
+        </span>
+    @endif
+
 </a>
