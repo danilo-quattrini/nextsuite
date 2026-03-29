5fa2ab352fa0b93537818d66627bbf9e9384b491
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2026-03-29 01:39:35 +0100

    style (forget-resete): change forget and reset password pages.

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -4,15 +4,9 @@
             <x-authentication-card-logo />
         </x-slot:logo>
         <x-slot:message>
-            <h3 class="mb-4">{{__('Lost your password?')}}</h3>
-            <p class="text-primary-grey text-base font-medium">{{__('Forgot your password? No problem!')}}</p>
+            <h4 class="mb-2">{{__('Lost your password?')}}</h4>
+            <p class="text-primary-grey font-normal text-sm">{{__('Forgot your password? No problem!')}}</p>
         </x-slot:message>
-
-        @session('status')
-            <div class="mb-4 font-medium text-sm text-green-600 dark:text-green-400">
-                {{ $value }}
-            </div>
-        @endsession
 
         <form method="POST" action="{{ route('password.email') }}">
             @csrf
@@ -21,13 +15,13 @@
                 <!-- EMAIL -->
                 <x-form.input-container>
                     <x-form.label-container label="{{ __('Email') }}"/>
-                    <x-input id="email" name="email" :value="old('email')" required autofocus autocomplete="username" type="email" right-icon="envelope" placeholder="Email" :error="$errors->has('email')"></x-input>
+                    <x-input id="email" name="email" :value="old('email')" required autofocus autocomplete="username" type="email" right-icon="envelope" placeholder="you@example.com" :error="$errors->has('email')"></x-input>
                     <x-input-error for="email"/>
                 </x-form.input-container>
 
                 <!-- RECOVE BUTTON -->
-                <div class="w-[470px] flex items-center justify-center mb-10">
-                    <x-button size="large" type="submit">
+                <div class="w-full flex items-center justify-center mb-10">
+                    <x-button size="full" type="submit">
                         {{ __('Recover') }}
                     </x-button>
                 </div>
