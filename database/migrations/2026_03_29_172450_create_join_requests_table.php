8f2af3787ccec47b0eeb173902e92228d64daa50
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2026-03-29 17:27:50 +0200

    feat (join-request-migration): add migration to join request from user who  wants to join to company.

---
+++ database/migrations/2026_03_29_172450_create_join_requests_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::create('join_requests', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('company_id')->constrained()->cascadeOnDelete();
+            $table->enum('status', ['pending', 'accepted', 'refused'])->default('pending');
+            $table->timestamps();
+
+            // A user can only have one pending request per company
+            $table->unique(['user_id', 'company_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('join_requests');
+    }
+};
