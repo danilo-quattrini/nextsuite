a5e10254caf1e3bfc2355f15f8c8bd6bc252abb3
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2026-04-14 10:17:43 +0200

    refactor (company-choice-modal): change company choice modal style.

--- resources/views/auth/company-choice.blade.php
+++ resources/views/auth/company-choice.blade.php
@@ -4,23 +4,40 @@
             <x-authentication-card-logo />
         </x-slot:logo>
         <x-slot:message>
-            <h3 class="mb-4">{{__('Warning')}}</h3>
-            <p class="text-primary-grey text-base font-medium">{{__('This is an important choice that can change the flow of events.')}}</p>
+            <h4 class="mb-sm">{{__('Warning')}}</h4>
+            <p class="text-primary-grey ">{{__('This is an important choice that can change the flow of events.')}}</p>
         </x-slot:message>
 
+        {{--ICON--}}
+        <div class="flex self-stretch justify-center items-center mb-sm ">
+            <div class="w-24 h-24 rounded-full bg-secondary-warning-100 flex items-center justify-center">
+                <x-heroicon
+                        name="exclamation-triangle"
+                        class="text-secondary-warning"
+                        size="2xl"
+                />
+            </div>
+        </div>
         {{--IMPORTANT MESSAGE--}}
-        <div class="flex self-stretch justify-center">
-            <h4>{{__('Do you want to create a company now?')}}</h4>
+        <div class="flex self-stretch justify-start">
+            <h5>{{__('Do you want to create a company now?')}}</h5>
         </div>
 
         {{-- BUTTON CHOICES --}}
-        <div class="flex self-stretch justify-between ">
-            <x-button size="large" variant="rest" href="{{ route('dashboard') }}">
-                {{__('Skip for now')}}
+        <div class="flex self-stretch justify-center gap-sm">
+            <x-button
+                    size="full"
+                    variant="rest"
+                    href="{{ route('dashboard') }}"
+            >
+                {{__('Skip')}}
             </x-button>
 
-            <x-button size="large" href="{{ route('auth.company.create') }}">
-                {{__('Create a company')}}
+            <x-button
+                    size="full"
+                    href="{{ route('auth.company.create') }}"
+            >
+                {{__('Create company')}}
             </x-button>
         </div>
     </x-authentication-card>
